3565b9953e69ec34236099d2c899483571c81c02
Anthony Emberley <emberley@mit.edu>
2016-11-15 18:39:11 -0500

    communication between phone and ECG working

--- app/src/main/java/com/helge/arrhythmiapt/LoginActivity.java
+++ app/src/main/java/com/helge/arrhythmiapt/LoginActivity.java
@@ -1,112 +1,333 @@
 package com.helge.arrhythmiapt;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v4.content.LocalBroadcastManager;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import com.parse.FindCallback;
+//import com.parse.LogInCallback;
+//import com.parse.ParseException;
+//import com.parse.ParseObject;
+//import com.parse.ParseQuery;
+//import com.parse.ParseUser;
+//import com.parse.SaveCallback;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//public class LoginActivity extends AppCompatActivity {
+//    /*
+//         Lets the patient login with username and password using the ParseUser object.
+//         Also includes a button for registering a new user.
+//     */
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//
+//        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+//        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+//        final Button bLogin = (Button) findViewById(R.id.bLogin);
+//        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+//
+//        assert etUsername != null;
+//        assert etPassword != null;
+//        assert bLogin != null;
+//        assert registerLink != null;
+//
+//        // Start register user activity
+//        registerLink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+//                LoginActivity.this.startActivity(registerIntent);
+//            }
+//        });
+//
+//        // Get entered username and password and send a login request in the background.
+//        bLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String username = etUsername.getText().toString();
+//                String password = etPassword.getText().toString();
+//                bLogin.setEnabled(false);
+//
+//                // Login in background and start the main menu on success.
+//                ParseUser.logInInBackground(username, password, new LogInCallback() {
+//                    public void done(ParseUser user, ParseException e) {
+//                        if (user != null) {
+//                            Intent i = new Intent(LoginActivity.this, MainMenu.class);
+//                            startActivity(i);
+//                        } else {
+//                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//            }
+//        });
+//
+//        // Fetch notes from Parse server in background and save locally
+//        fetchNotes();
+//
+//        SignalProcessing signalProcessing = new SignalProcessing(this);
+//        try {
+//            signalProcessing.readECG();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        signalProcessing.detect_and_classify();
+//    }
+//
+//    // Fetch notes from database
+//    private void fetchNotes() {
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Note");
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            @Override
+//            public void done(List<ParseObject> objects, ParseException e) {
+//                ParseObject.pinAllInBackground(objects, new SaveCallback() {
+//                    @Override
+//                    public void done(ParseException e) {
+//                        sendDoneBroadcast();
+//                    }
+//                });
+//
+//            }
+//        });
+//    }
+//
+//    // When fetchNote() is done, a broadcast is sent such that the NotesList will be update
+//    private void sendDoneBroadcast() {
+//        Intent intent = new Intent("doneFetchingData");
+//        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+//    }
+//
+//
+//}
 
+
+
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.parse.FindCallback;
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.io.IOException;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    /*
-         Lets the patient login with username and password using the ParseUser object.
-         Also includes a button for registering a new user.
-     */
+
+    private LineGraphSeries<DataPoint> mSeries;
+    private double graphLastXValue = 0d;
+    private double graphLastYValue = 0d;
+    public final String ACTION_USB_PERMISSION = "com.a2009pink.revive.USB_PERMISSION";
+    Button startButton, sendButton, clearButton, stopButton;
+    TextView textView;
+    EditText editText;
+    UsbManager usbManager;
+    UsbDevice device;
+    UsbSerialDevice serialPort;
+    UsbDeviceConnection connection;
+
+
+    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+        @Override
+        public void onReceivedData(byte[] arg0) {
+            String data = null;
+            Integer dataInt = null;
+            try {
+                data = new String(arg0, "UTF-8");
+                //data.concat("/n");
+                try {
+                    dataInt = Integer.parseInt(data);
+
+                    graphLastXValue += 1d;
+                    mSeries.appendData(new DataPoint(graphLastXValue, dataInt), true, 1000);
+                } catch(NumberFormatException e) {
+                } catch(NullPointerException e) {
+                }
+
+                tvAppend(textView, data);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    };
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    connection = usbManager.openDevice(device);
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                    if (serialPort != null) {
+                        if (serialPort.open()) { //Set Serial Connection Parameters.
+                            setUiEnabled(true);
+                            serialPort.setBaudRate(9600);
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback);
+                            tvAppend(textView,"Serial Connection Opened!\n");
+
+                        } else {
+                            Log.d("SERIAL", "PORT NOT OPEN");
+                        }
+                    } else {
+                        Log.d("SERIAL", "PORT IS NULL");
+                    }
+                } else {
+                    Log.d("SERIAL", "PERM NOT GRANTED");
+                }
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+                onClickStart(startButton);
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+                onClickStop(stopButton);
+
+            }
+        }
+
+        ;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
+        startButton = (Button) findViewById(R.id.buttonStart);
+        sendButton = (Button) findViewById(R.id.buttonSend);
+        clearButton = (Button) findViewById(R.id.buttonClear);
+        stopButton = (Button) findViewById(R.id.buttonStop);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        setUiEnabled(false);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(broadcastReceiver, filter);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bLogin = (Button) findViewById(R.id.bLogin);
-        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        mSeries = new LineGraphSeries<>();
+        System.out.println(graph.getViewport().getMaxX(true));
+        System.out.println("hello");
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(40);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(1024);
+        graph.addSeries(mSeries);
+        System.out.println(graph.getSeries());
 
-        assert etUsername != null;
-        assert etPassword != null;
-        assert bLogin != null;
-        assert registerLink != null;
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+//                new DataPoint(0, 1),
+//                new DataPoint(1, 5),
+//                new DataPoint(2, 3),
+//                new DataPoint(3, 2),
+//                new DataPoint(4, 6)
+//        });
+//        graph.addSeries(series);
 
-        // Start register user activity
-        registerLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
+
+    }
+
+    public void setUiEnabled(boolean bool) {
+        startButton.setEnabled(!bool);
+        sendButton.setEnabled(bool);
+        stopButton.setEnabled(bool);
+        textView.setEnabled(bool);
+
+    }
+
+    public void onClickStart(View view) {
+
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                device = entry.getValue();
+                int deviceVID = device.getVendorId();
+                if (deviceVID == 0x2341)//Arduino Vendor ID
+                {
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+
+                if (!keep)
+                    break;
             }
-        });
-
-        // Get entered username and password and send a login request in the background.
-        bLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                bLogin.setEnabled(false);
-
-                // Login in background and start the main menu on success.
-                ParseUser.logInInBackground(username, password, new LogInCallback() {
-                    public void done(ParseUser user, ParseException e) {
-                        if (user != null) {
-                            Intent i = new Intent(LoginActivity.this, MainMenu.class);
-                            startActivity(i);
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
-
-        // Fetch notes from Parse server in background and save locally
-        fetchNotes();
-
-        SignalProcessing signalProcessing = new SignalProcessing(this);
-        try {
-            signalProcessing.readECG();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        signalProcessing.detect_and_classify();
+
+
     }
 
-    // Fetch notes from database
-    private void fetchNotes() {
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Note");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                ParseObject.pinAllInBackground(objects, new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        sendDoneBroadcast();
-                    }
-                });
+    public void onClickSend(View view) {
+        String string = editText.getText().toString();
+        serialPort.write(string.getBytes());
+        tvAppend(textView, "\nData Sent : " + string + "\n");
 
+    }
+
+    public void onClickStop(View view) {
+        setUiEnabled(false);
+        serialPort.close();
+        tvAppend(textView,"\nSerial Connection Closed! \n");
+
+    }
+
+    public void onClickClear(View view) {
+        textView.setText(" ");
+        graphLastXValue += .1;
+        graphLastYValue += .1;
+        mSeries.appendData(new DataPoint(graphLastXValue, graphLastYValue), true, 40);
+
+    }
+
+    private void tvAppend(TextView tv, CharSequence text) {
+        final TextView ftv = tv;
+        final CharSequence ftext = text;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ftv.append(ftext);
             }
         });
     }
-
-    // When fetchNote() is done, a broadcast is sent such that the NotesList will be update
-    private void sendDoneBroadcast() {
-        Intent intent = new Intent("doneFetchingData");
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-    }
-
-
 }
