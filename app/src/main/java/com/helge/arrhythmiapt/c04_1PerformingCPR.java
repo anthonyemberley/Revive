15b32b6eee6372fcd556f5e9555ad32991a9a455
Anthony Emberley <emberley@mit.edu>
2016-11-17 03:40:10 -0500

    added home button on last cpr

--- app/src/main/java/com/helge/arrhythmiapt/c04_1PerformingCPR.java
+++ app/src/main/java/com/helge/arrhythmiapt/c04_1PerformingCPR.java
@@ -1,7 +1,9 @@
 package com.helge.arrhythmiapt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class c04_1PerformingCPR extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class c04_1PerformingCPR extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c04_1_performing_cpr);
+    }
+
+    public void homeButtonPressed(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
